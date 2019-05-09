e771aa8f1b286d830792b3da879fc341d5352870
Kunal7008 <kunal.bhatta513@gmail.com>
2019-05-09 20:47:26 +0530

    first commit

---
+++ src/com/first/build/GetRun.java
@@ -0,0 +1,19 @@
+package com.first.build;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GetRun {
+	WebDriver driver;
+@Test
+public void getMyCode() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\KUNAL BHATTA\\Downloads\\chromedriver.exe");
+	driver= new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://cricbuzz.com");
+	WebElement element=driver.findElement(By.linkText("Schedule"));
+}
+}
